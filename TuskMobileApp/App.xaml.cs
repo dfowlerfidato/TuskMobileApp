6139a09622e44727a5319ef5c5d7e1cddc355453
Donald <dfowler@fidatocorp.com>
2017-12-26 11:41:47 -0800

    Added appcenter code to App.xaml.cs

--- TuskMobileApp/App.xaml.cs
+++ TuskMobileApp/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 using Xamarin.Forms;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace TuskMobileApp
 {
     public partial class App : Application
@@ -18,7 +22,9 @@ namespace TuskMobileApp
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            AppCenter.Start("android=b960fb8a-3cba-4f27-834e-54568731f3bb;" + "uwp={Your UWP App secret here};" +
+                   "ios={Your iOS App secret here}",
+                   typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
